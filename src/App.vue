b4d6b2ac935efd18379502383ad2b8f7f82a3895
usssagn1 <zaytsevdima031107@gmail.com>
2024-02-13 22:16:39 +0200

    test gh-pages

---
+++ src/App.vue
@@ -0,0 +1,243 @@
+<script>
+
+</script>
+
+<template>
+  <div id="luxy" class="wrapp">
+    <header class="header">
+      <h1 class="title" data-splitting>
+        <span class="title__paralax">Portfolio website</span><span class="stroke">Hi, i'm Dima.</span>
+      </h1>
+      <div class="header__img">
+        <img src="../public/about__img.jpg" alt="">
+      </div>
+      <div class="header__marq">
+        <div class="header__marq-wrapp">
+          <span class="header__marq-text">My name is Dima
+            <span class="header__marq-star">
+              <svg class="header__marq-star-svg" version="1.1" id="sparkle_x5F_stars" xmlns="http://www.w3.org/2000/svg" x="0" y="0" viewBox="0 0 128 128" style="enable-background:new 0 0 128 128" xml:space="preserve"><g id="row1"><path id="icon:5" d="m128 62.6-38.3 6.6c-9.9 1.7-17.6 9.6-18.9 19.6L65.4 128l-6.6-38.3c-1.7-9.9-9.6-17.6-19.6-18.9L0 65.4l38.3-6.6c9.9-1.7 17.6-9.6 18.9-19.6L62.6 0l6.6 38.3c1.7 9.9 9.6 17.6 19.6 18.9l39.2 5.4z" style="fill:#4d4d4d"/></g></svg>
+            </span>
+          </span>
+          <span class="header__marq-text">i'm web designer
+            <span class="header__marq-star">
+              <svg class="header__marq-star-svg" version="1.1" id="sparkle_x5F_stars" xmlns="http://www.w3.org/2000/svg" x="0" y="0" viewBox="0 0 128 128" style="enable-background:new 0 0 128 128" xml:space="preserve"><g id="row1"><path id="icon:5" d="m128 62.6-38.3 6.6c-9.9 1.7-17.6 9.6-18.9 19.6L65.4 128l-6.6-38.3c-1.7-9.9-9.6-17.6-19.6-18.9L0 65.4l38.3-6.6c9.9-1.7 17.6-9.6 18.9-19.6L62.6 0l6.6 38.3c1.7 9.9 9.6 17.6 19.6 18.9l39.2 5.4z" style="fill:#4d4d4d"/></g></svg>
+            </span>
+          </span>
+          <span class="header__marq-text">Portfolio design
+            <span class="header__marq-star">
+              <svg class="header__marq-star-svg" version="1.1" id="sparkle_x5F_stars" xmlns="http://www.w3.org/2000/svg" x="0" y="0" viewBox="0 0 128 128" style="enable-background:new 0 0 128 128" xml:space="preserve"><g id="row1"><path id="icon:5" d="m128 62.6-38.3 6.6c-9.9 1.7-17.6 9.6-18.9 19.6L65.4 128l-6.6-38.3c-1.7-9.9-9.6-17.6-19.6-18.9L0 65.4l38.3-6.6c9.9-1.7 17.6-9.6 18.9-19.6L62.6 0l6.6 38.3c1.7 9.9 9.6 17.6 19.6 18.9l39.2 5.4z" style="fill:#4d4d4d"/></g></svg>
+            </span>
+          </span>
+          <span class="header__marq-text">website
+            <span class="header__marq-star">
+              <svg class="header__marq-star-svg" version="1.1" id="sparkle_x5F_stars" xmlns="http://www.w3.org/2000/svg" x="0" y="0" viewBox="0 0 128 128" style="enable-background:new 0 0 128 128" xml:space="preserve"><g id="row1"><path id="icon:5" d="m128 62.6-38.3 6.6c-9.9 1.7-17.6 9.6-18.9 19.6L65.4 128l-6.6-38.3c-1.7-9.9-9.6-17.6-19.6-18.9L0 65.4l38.3-6.6c9.9-1.7 17.6-9.6 18.9-19.6L62.6 0l6.6 38.3c1.7 9.9 9.6 17.6 19.6 18.9l39.2 5.4z" style="fill:#4d4d4d"/></g></svg>
+            </span>
+          </span>
+        </div>
+      </div>
+
+    </header>
+
+    <main class="main">
+
+      <section class="about">
+
+        <div class="about__wrapp">
+
+          <div class="about__img">
+
+            <img src="../public/header__img.jpg" alt="about_img">
+
+          </div>
+
+          <div class="about__text">
+
+            <h2 class="section-title">abo<span class="stroke">ut</span>
+
+              <span class="section-title__square"></span>
+
+            </h2>
+
+            <p class="about__desc">
+
+              I am a web developer with a strong skill set in HTML, CSS, and JavaScript. <br><br>
+
+              I also have expertise in utilizing tools like GULP, Bootstrap, and SCSS/SASS to enhance web development projects.
+
+
+
+              <br><br>
+
+              My proficiency in front-end technologies allows me to create visually appealing and responsive websites,
+
+              while my knowledge of these frameworks and preprocessors helps streamline the development process for efficient
+
+              and maintainable code. I am dedicated to staying up-to-date with the latest web development trends and technologies
+
+              to deliver high-quality web solutions.
+
+            </p>
+
+          </div>
+
+        </div>
+
+      </section>
+
+      <section class="skills">
+
+        <div class="content">
+
+          <h2 class="section-title">My<span class="stroke">skills</span>
+
+            <span class="section-title__square"></span>
+
+          </h2>
+          <ul class="skills__list">
+            <li class="skills__item">
+              <span class="skills__num" data-speed="-200">HTML</span>
+              <p class="skills__desc">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore, dolor.</p>
+            </li>
+            <li class="skills__item">
+              <span class="skills__num" data-speed="-150">CSS/SCSS</span>
+              <p class="skills__desc">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore, dolor.</p>
+            </li>
+            <li class="skills__item">
+              <span class="skills__num" data-speed="-150">JS</span>
+              <p class="skills__desc">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore, dolor.</p>
+            </li>
+            <li class="skills__item">
+              <span class="skills__num" data-speed="-150">GULP</span>
+              <p class="skills__desc">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore, dolor.</p>
+            </li>
+            <li class="skills__item">
+              <span class="skills__num" data-speed="-230">BOOTSTRAP</span>
+              <p class="skills__desc">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore, dolor.</p>
+            </li>
+            <li class="skills__item">
+              <span class="skills__num" data-speed="-170">GIT</span>
+              <p class="skills__desc">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore, dolor.</p>
+            </li>
+          </ul>
+        </div>
+      </section>
+      <section class="work">
+        <div class="content">
+          <h2 class="section-title">portfo<span class="stroke">lio</span>
+            <span class="section-title__square"></span>
+          </h2>
+        </div>
+        <div class="work__wrapp">
+          <a href="#" class="work__item" data-speed="-110">
+            <div class="work__item-img">
+              <img src="../public/work/1.jpg" alt="">
+            </div>
+            <div class="work__item-text">
+              <h4 class="work__item-title">LoftHouse</h4>
+              <p class="work__item-desc">"LoftHouse" is a modern residential complex that offers high-quality housing in innovative and stylish architecture. This residential complex grabs attention with its unique design, amenities, and a distinctive concept of comfortable living.</p>
+            </div>
+          </a>
+          <a href="#" class="work__item" data-speed="-100">
+            <div class="work__item-img">
+              <img src="../public/work/2.jpg" alt="">
+            </div>
+            <div class="work__item-text">
+              <h4 class="work__item-title">AirPodsMax</h4>
+              <p class="work__item-desc">The AirPods Max is Apple's premium over-ear wireless headphones that redefine audio excellence. These headphones combine cutting-edge technology with a sleek design, offering an unparalleled listening experience for audiophiles and Apple enthusiasts alike.</p>
+            </div>
+          </a>
+          <a href="#" class="work__item" data-speed="-130">
+            <div class="work__item-img">
+              <img src="../public/work/3.jpg" alt="">
+            </div>
+            <div class="work__item-text">
+              <h4 class="work__item-title">Despire</h4>
+              <p class="work__item-desc">"Despire Furniture" is an online platform that offers a wide range of stylish and contemporary furniture pieces for modern living spaces. With a focus on quality craftsmanship and innovative design, Despire Furniture aims to enhance the aesthetics and functionality of homes and offices.</p>
+            </div>
+          </a>
+          <a href="#" class="work__item" data-speed="-100">
+            <div class="work__item-img">
+              <img src="../public/work/4.jpg" alt="">
+            </div>
+            <div class="work__item-text">
+              <h4 class="work__item-title">Globe</h4>
+              <p class="work__item-desc">"Globe Travel Agency" is a leading travel agency specializing in organizing trips and tours worldwide. This agency offers a wide range of services for travelers looking to explore new places and create unforgettable experiences.</p>
+            </div>
+          </a>
+        </div>
+      </section>
+      <section class="serv">
+        <div class="content">
+          <h2 class="section-title">Li<span class="stroke">nks</span>
+            <span class="section-title__square"></span>
+          </h2>
+          <div class="serv__list">
+            <a href="">
+              <div class="serv__item">
+                <span class="serv__item-arrow" data-speed="500">
+                  <img src="../public/arrow.svg" alt="">
+                </span>
+                <div class="serv__item-desc">
+                  <span class="serv__item-text">/Github</span>
+                </div>
+              </div>
+            </a>
+            <a href="">
+              <div class="serv__item">
+                <span class="serv__item-arrow" data-speed="400">
+                  <img src="../public/arrow.svg" alt="">
+                </span>
+                <div class="serv__item-desc">
+                  <span class="serv__item-text">/instagram</span>
+                </div>
+              </div>
+            </a>
+            <a href="">
+              <div class="serv__item">
+                <span class="serv__item-arrow" data-speed="800">
+                  <img src="../public/arrow.svg" alt="">
+                </span>
+                <div class="serv__item-desc">
+                  <span class="serv__item-text">/telegram</span>
+                </div>
+              </div>
+            </a>
+            <a href="">
+              <div class="serv__item">
+                <span class="serv__item-arrow" data-speed="600">
+                  <img src="../public/arrow.svg" alt="">
+                </span>
+                <div class="serv__item-desc">
+                  <span class="serv__item-text">/facebook</span>
+                </div>
+              </div>
+            </a>
+
+          </div>
+        </div>
+      </section>
+    </main>
+    <footer class="footer">
+      <div class="footer__div">
+        <span data-speed="-300">l</span>
+        <span data-speed="100">e</span>
+        <span data-speed="300">t</span>
+        <span data-speed="-300">'</span>
+        <span data-speed="350">s</span>
+        <span data-speed="200"> </span>
+        <span data-speed="-300">w</span>
+        <span data-speed="110">o</span>
+        <span data-speed="-100">r</span>
+        <span data-speed="-210">k</span>
+        <span data-speed="150">;</span>
+        <span data-speed="-140">)</span>
+      </div>
+    </footer>
+
+  </div>
+
+</template>
+
+<style src="./assets/main.min.css">
+
+</style>
